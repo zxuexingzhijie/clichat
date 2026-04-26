fe11f31c2cd403f6e02a43ba6840a2136fdbbe48
zhouyanjue <zhouyanjue@baidu.com>
2026-04-27 01:17:38 +0800

    refactor: export EventBus type from event-bus module

--- src/events/event-bus.ts
+++ src/events/event-bus.ts
@@ -1,4 +1,5 @@
 import mitt from 'mitt';
 import type { DomainEvents } from './event-types';
 
-export const eventBus = mitt<DomainEvents>();
+export type EventBus = ReturnType<typeof mitt<DomainEvents>>;
+export const eventBus: EventBus = mitt<DomainEvents>();
